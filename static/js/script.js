404c6bda9957c5042eecaf9530534255db910058
Nikita Kuznetsov <123502774+Lacolle87@users.noreply.github.com>
2023-05-05 02:20:56 +0300

    add rainbow, wave, sync

--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,38 @@
-$(function() {
-  // Reverse the values of the speed range slider when the page loads
-  var speedInput = document.getElementById('speed');
-  var reversedValue = parseInt(speedInput.max) + parseInt(speedInput.min) - parseInt(speedInput.value);
-  speedInput.setAttribute('value', reversedValue);
+$(function () {
+    const speedInput = document.getElementById('speed');
+    const reversedValue = parseInt(speedInput.max) + parseInt(speedInput.min) - parseInt(speedInput.value);
+    speedInput.value = reversedValue;
 
-  $('#rainbow_mode').change(function() {
-    var rainbowMode = $(this).is(':checked');
-    if (rainbowMode) {
-      $('#speed').prop('disabled', false);
-      $('#red, #green, #blue').prop('disabled', true);
-      $.post('/', {
-        'rainbow_mode': 'rainbow'
-      });
-    } else {
-      $('#speed').prop('disabled', true);
-      $('#red, #green, #blue').prop('disabled', false);
-      $.post('/', {
-        'rainbow_mode': 'solid'
-      });
-    }
-  });
-
-  $('#led-form').submit(function(event) {
-    event.preventDefault();
-    var red = $('#red').val();
-    var green = $('#green').val();
-    var blue = $('#blue').val();
-    var speed = $('#speed').val();
-    var mode = $('#rainbow_mode').is(':checked') ? 'rainbow' : 'solid';
-    var message = `rgb:${red},${green},${blue}`;
-    if (mode === 'rainbow') {
-      // Reverse the speed value before appending it to the message
-      var reversedSpeed = parseInt(speedInput.max) + parseInt(speedInput.min) - parseInt(speed);
-      message += `:rainbow.${reversedSpeed}`;
-    }
-    $.post('/', {
-      'color': message,
-      'mode': mode
+    $('#rainbow_mode').change(function () {
+        const rainbowMode = $(this).is(':checked');
+        const mode = rainbowMode ? 'rainbow' : 'solid';
+        $('#speed').prop('disabled', !rainbowMode);
+        $('#red, #green, #blue').prop('disabled', rainbowMode);
+        $.post('/', {'rainbow_mode': mode});
     });
-  });
+
+    $('#led-form').submit(function (event) {
+        event.preventDefault();
+        var red = $('#red').val();
+        var green = $('#green').val();
+        var blue = $('#blue').val();
+        var speed = $('#speed').val();
+        var mode = $('#rainbow_mode').is(':checked') ? 'rainbow' : 'solid';
+        var message = `rgb:${red},${green},${blue}`;
+        if (mode === 'rainbow') {
+            var rainbowEffect = $('#rainbow_effect').val();
+            if (rainbowEffect === 'rainbow') {
+                message = 'rainbow';
+            } else if (rainbowEffect === 'wave') {
+                message = 'wave';
+            } else if (rainbowEffect === 'sync') {
+                message = 'sync';
+            }
+            message += `.${speed}`;
+        }
+        $.post('/', {
+            'color': message,
+            'mode': mode
+        });
+    });
 });
